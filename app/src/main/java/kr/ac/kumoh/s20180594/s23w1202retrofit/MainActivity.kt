8e44537a58c211a6f99ffdfb4675fd495923b2eb
yhss1015 <dudgns8481@gmail.com>
2023-11-30 13:20:24 +0900

    🚀 SongList 완성

--- app/src/main/java/kr/ac/kumoh/s20180594/s23w1202retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180594/s23w1202retrofit/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,7 +21,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.s20180594.s23w1202retrofit.ui.theme.S23W1202RetrofitTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,10 +53,39 @@ fun MainScreen(viewModel: SongViewModel){
 }
 
 @Composable
-fun SongList(list: List<Song>){
-    Column {
-        Text(list.toString())
+fun SongList(list: List<Song>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp)
+    ) {
+        items(list) { song ->
+            SongItem(song)
+        }
     }
+}
+
+@Composable
+fun SongItem(song: Song) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TextTitle(song.title)
+        TextSinger(song.singer)
+    }
+}
+
+
+@Composable
+fun TextTitle(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String) {
+    Text(singer, fontSize = 20.sp)
 }
 
 
